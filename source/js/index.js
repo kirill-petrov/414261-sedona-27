a8baa568b4eafc82e7eb9a6da860bd24a5d1af66
kirill petrov <mail@kirill-petrov.ru>
2023-01-24 11:19:29 +0300

    улучшено меню навигации

--- source/js/index.js
+++ source/js/index.js
@@ -1,14 +1,25 @@
 const siteNav = document.querySelector(".site-nav");
 const toggleMenu = document.querySelector(".site-nav__toggle-menu");
 
-siteNav.classList.remove("site-nav--no-js");
+const showMenu = () => {
+  siteNav.classList.remove("site-nav--closed");
+  siteNav.classList.add("site-nav--opened");
+};
+
+const hideMenu = () => {
+  siteNav.classList.remove("site-nav--opened");
+  siteNav.classList.add("site-nav--closed");
+};
+
+siteNav.classList.remove("js-nojs");
+toggleMenu.classList.remove("site-nav__toggle-menu--js-hide");
+
+hideMenu();
 
 toggleMenu.addEventListener("click", () => {
-  if (siteNav.classList.contains('site-nav--closed')) {
-    siteNav.classList.remove('site-nav--closed');
-    siteNav.classList.add('site-nav--opened');
+  if (siteNav.classList.contains("site-nav--closed")) {
+    showMenu();
   } else {
-    siteNav.classList.add('site-nav--closed');
-    siteNav.classList.remove('site-nav--opened');
+    hideMenu();
   }
 });
